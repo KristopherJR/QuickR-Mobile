7c4e1780a5ed9cab6a6a8b7ed52306f162908921
Kristopher Randle <rank1_18@uni.worc.ac.uk>
2022-02-21 18:39:57 +0000

    work on camera

--- QuickRMobile/app/src/main/java/com/example/quickrmobile/MainActivity.kt
+++ QuickRMobile/app/src/main/java/com/example/quickrmobile/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.quickrmobile
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn_scan = (R.id.scan_button) as Button
+
+        btn_scan.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            }
+        }
+
+
+
     }
 }
