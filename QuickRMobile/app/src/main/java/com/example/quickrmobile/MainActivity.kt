a07bae6085190be23f5ccf11a50aa53fce3b6e59
Kristopher Randle <rank1_18@uni.worc.ac.uk>
2022-02-22 15:25:50 +0000

    QR Scanning Functionality added

--- QuickRMobile/app/src/main/java/com/example/quickrmobile/MainActivity.kt
+++ QuickRMobile/app/src/main/java/com/example/quickrmobile/MainActivity.kt
@@ -5,38 +5,104 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private const val CAMERA_PERMISSION_CODE = 1
-        private const val CAMERA_REQUEST_CODE = 2
-    }
+private const val CAMERA_REQUEST_CODE = 101
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var codeScanner: CodeScanner
+
+    private lateinit var scanner_view: CodeScannerView
+    private lateinit var tv_textview: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scanner_view = findViewById(R.id.scanner_view)
+        tv_textview = findViewById(R.id.tv_textView)
 
-        var btn_scan = (R.id.scan_button) as Button
+        setupPermissions()
+        codeScanner()
+    }
 
-        btn_scan.setOnClickListener {
-            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-            {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    private fun codeScanner()
+    {
+        codeScanner = CodeScanner(this, scanner_view)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
 
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread{
+                    tv_textview.text = it.text
+                }
             }
-            else
-            {
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+
+            errorCallback = ErrorCallback {
+                runOnUiThread{
+                    Log.e("Main", "Camera initialisation error: ${it.message}")
+                }
+            }
+            scanner_view.setOnClickListener{
+                codeScanner.startPreview()
             }
         }
+    }
 
+    override fun onResume(){
+        super.onResume()
+        codeScanner.startPreview()
+    }
 
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions(){
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
 
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+        when (requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "You need the camera permission to be able to use this app!", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    // successful
+                }
+            }
+        }
+    }
+
 }
