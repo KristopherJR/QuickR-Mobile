0034d20fd37611af8317d82e76b5f9e2c24dd4f0
Kristopher Randle <rank1_18@uni.worc.ac.uk>
2022-02-24 20:59:54 +0000

    Version: 1.14, Deconstructing QR Code data. program currently retrieves the enrolled session ids of the student and the appropriate session documents.

--- QuickRMobile/app/src/main/java/com/example/quickrmobile/MainActivity.kt
+++ QuickRMobile/app/src/main/java/com/example/quickrmobile/MainActivity.kt
@@ -10,13 +10,17 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.model.Document
 
-private const val STUDENT_ID_KEY = "users"
+private const val USER_ID_KEY = "users"
 private const val PASSWORD_KEY = "password"
 
+private const val STUDENT_ID_KEY = "student_id"
+
 // AUTHOR: Kristopher J Randle
-// VERSION: 1.13
+// VERSION: 1.14
 class MainActivity : AppCompatActivity()
 {
     private lateinit var loginButton: Button
@@ -28,6 +32,16 @@ class MainActivity : AppCompatActivity()
 
     private lateinit var mDocRef : DocumentReference
 
+    companion object
+    {
+        private lateinit var studentDoc : DocumentSnapshot
+
+        fun getStudentDocumentSnapshot(): DocumentSnapshot
+        {
+            return studentDoc
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -36,15 +50,17 @@ class MainActivity : AppCompatActivity()
         studentIDText = findViewById(R.id.textbox_studentID)
         passwordText = findViewById(R.id.textbox_password)
 
-
         loginButton.setOnClickListener{
+            //val intent = Intent(this,QRScannerActivity::class.java)
+            //intent.putExtra("student_id", "18008890")
+            //startActivity(intent)
+
+            // MAKE SURE TO RE-ENABLE THE LINE BELOW AFTER SCANNER FUNCTIONALITY IS SET-UP
             checkLoginDetails()
         }
     }
 
     private fun checkLoginDetails(){
-
-
         if(studentIDText.text.count() == 8 && studentIDText.text.matches("^[0-9]+\$".toRegex())){
             idValid = true
         }
@@ -64,7 +80,7 @@ class MainActivity : AppCompatActivity()
         if(idValid && passwordValid){
             Toast.makeText(this, "Logging in...", Toast.LENGTH_SHORT).show()
             // query database to retrieve the student id
-            mDocRef = FirebaseFirestore.getInstance().collection("users").document(studentIDText.text.toString())
+            mDocRef = FirebaseFirestore.getInstance().collection(USER_ID_KEY).document(studentIDText.text.toString())
 
             mDocRef.get().addOnSuccessListener{ document ->
                     if (document != null)
@@ -74,9 +90,10 @@ class MainActivity : AppCompatActivity()
                             // query database to retrieve the password associated with the id
                             if(passwordText.text.toString() == document.getString(PASSWORD_KEY).toString())
                             {
+                                studentDoc = document
                                 // load the QR Scanner screen
                                 val intent = Intent(this,QRScannerActivity::class.java)
-                                intent.putExtra("student_id", studentIDText.text.toString())
+                                intent.putExtra(STUDENT_ID_KEY, studentIDText.text)
                                 startActivity(intent)
                             }
 
--- QuickRMobile/app/src/main/java/com/example/quickrmobile/QRScannerActivity.kt
+++ QuickRMobile/app/src/main/java/com/example/quickrmobile/QRScannerActivity.kt
@@ -1,10 +1,12 @@
 package com.example.quickrmobile
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.BoringLayout
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -18,24 +20,59 @@ import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.inject.Deferred
+import java.sql.Time
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDate.of
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 import java.util.jar.Manifest
+import javax.xml.transform.Result
 
 
 private const val CAMERA_REQUEST_CODE = 101
+private const val STUDENT_ID_KEY = "student_id"
+private const val SESSIONS_KEY = "sessions"
+private const val SESSION_ID_KEY = "session_id"
+private const val MODULE_CODE_KEY = "module_code"
+private const val DAY_KEY = "day"
+private const val ENROLLED_SESSION_IDS_KEY = "enrolled_session_ids"
 
 // AUTHOR: Kristopher J Randle
-// VERSION: 1.13
+// VERSION: 1.14
 class QRScannerActivity : AppCompatActivity()
 {
+    private lateinit var loggedInStudentID: String
+
     private lateinit var tvtextview: TextView
     private lateinit var scanbutton: Button
     private lateinit var scannerview: CodeScannerView
     private lateinit var codeScanner: CodeScanner
 
+    private lateinit var studentDocumentSnapshot: DocumentSnapshot
+    private val sessionsDocuments: MutableList<DocumentSnapshot> = mutableListOf<DocumentSnapshot>()
+
+    private val fireStore = FirebaseFirestore.getInstance()
+
+    private lateinit var splitQRCode: List<String>
+
+    private lateinit var qrModuleCode: String
+    private lateinit var qrDate: LocalDate
+    private lateinit var qrTime: LocalTime
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loggedInStudentID = intent.getStringExtra(STUDENT_ID_KEY).toString()
+        studentDocumentSnapshot = MainActivity.getStudentDocumentSnapshot()
 
         tvtextview = findViewById(R.id.tv_textView)
         scanbutton = findViewById(R.id.scan_button)
@@ -45,18 +82,77 @@ class QRScannerActivity : AppCompatActivity()
         codeScanner = CodeScanner(this, scannerview)
 
         scanbutton.setOnClickListener{
-
             startScanner()
         }
+    }
+
+    private fun deconstructQRCode(it: com.google.zxing.Result)
+    {
+        tvtextview.text = it.text
+        splitQRCode = it.text.split("_")
+
+
+        qrModuleCode = splitQRCode[0]
+
+        var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        var date = LocalDate.parse(splitQRCode[1], formatter)
+
+        Log.w(TAG, splitQRCode.toString())
+        Log.w(TAG, date.toString())
+
+    }
+
+    private fun validateQRCode(it: com.google.zxing.Result)
+    {
+        // RETRIEVE the enrolled session ids for the student:
+        val enrolledSessionIds: List<Int> = studentDocumentSnapshot.get(ENROLLED_SESSION_IDS_KEY) as List<Int> // this works!! - retrieves List<Int> = [1,2,3,4]
+        // GET all of the SESSION documents that the USER is enrolled on
+        retrieveSessionDocuments(enrolledSessionIds)
+        // FIND the SESSION document that matches the module_code scanned in the QR
+
+
+
+        // STORE the day of that SESSION and check it matches the current day
+        // STORE the start_time and end_time of the SESSION and check the current time is between them
+        // IF all checks pass, CREATE a new AttendedSessionLog Document to register the attendance
+
+    }
+
+    private fun retrieveSessionDocuments(ids: List<Int>)
+    {
+        val sessionsRef = fireStore.collection(SESSIONS_KEY)
+
+        sessionsRef
+            .whereIn(SESSION_ID_KEY, ids)
+            .get()
+            .addOnSuccessListener { documents ->
+                for(document in documents) {
+                    sessionsDocuments.add(document)
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener{ exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+            .addOnCompleteListener {
+                verifySession()
+            }
+    }
+
+    private fun verifySession()
+    {
+       // for(document in sessionsDocuments){
+       //     if(document.getString(MODULE_CODE_KEY) == )
+       // }
     }
 
     private fun startScanner()
     {
         setupPermissions()
-        codeScanner()
+        startCodeScanner()
     }
 
-    private fun codeScanner()
+    private fun startCodeScanner()
     {
         scannerview.visibility = View.VISIBLE
         tvtextview.visibility = View.VISIBLE
@@ -65,13 +161,14 @@ class QRScannerActivity : AppCompatActivity()
             formats = CodeScanner.ALL_FORMATS
 
             autoFocusMode = AutoFocusMode.SAFE
-            scanMode = ScanMode.CONTINUOUS
+            scanMode = ScanMode.SINGLE
             isAutoFocusEnabled = true
             isFlashEnabled = false
 
             decodeCallback = DecodeCallback {
                 runOnUiThread{
-                    tvtextview.text = it.text
+                    scannerview.visibility = View.GONE
+                    deconstructQRCode(it)
                 }
             }
 
